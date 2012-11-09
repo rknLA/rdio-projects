3c5289f2a0d377c9b8c6d6663464ba2c868e3a3a
R. Kevin Nelson <rkn@rd.io>
2012-11-09 15:12:58 -0800

    Handle authentication.

--- friendFeed/js/friendFeed.js
+++ friendFeed/js/friendFeed.js
@@ -1,4 +1,4 @@
-/*globals R */
+/*globals R, $ */
 (function() {
   var oldFriendHistory = window.rdioFriendFeed;
 
@@ -6,16 +6,54 @@
     console.log("[FriendFeed] " + text);
   };
 
+  var showLogin = (function() {
+    var loginShowing = false;
+
+    var loginPressed = function() {
+      log("you hit the authentication button");
+      $('#authenticationButton').html('Authenticating')
+        .attr('disabled', 'disabled');
+      R.authenticate(authenticationComplete);
+    };
+
+    var authenticationComplete = function(success) {
+      if (success) {
+        log("authentication successful!");
+      } else {
+        log("authentication failed");
+      }
+    };
+
+    return function() {
+      if (!loginShowing) {
+        log("Should show an authorize button now.");
+        $('#notice')
+          .append('<button id="authenticationButton">Authenticate</button>');
+        $('#authenticationButton').click(loginPressed);
+        loginShowing = true;
+      } else {
+        log("Login is already showing! Do nothing.");
+      }
+    };
+  })();
+
   window.rdioFriendFeed = {
     noConflict: oldFriendHistory,
 
     onRdioReady: function() {
       log("Rdio is ready!");
-      
+      var user = R.currentUser.vanityName;
+      var logStr = "Viewing as " + user + " ";
+      if (R.authenticated()) {
+        log(logStr + "and also authenticated");
+      } else {
+        log(logStr + "but not authenticated");
+        showLogin();
+      }
     },
 
     onPubSub: function() {
-    }
+    },
   }
 
   R.ready(rdioFriendFeed.onRdioReady);
