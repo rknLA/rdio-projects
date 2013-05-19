b73244b2a3159099f30e12b80cdc0e156ef6b0ab
R. Kevin Nelson <rkn@rd.io>
2013-05-18 17:14:28 -0700

    Fix auth.
    Still suuuper uggsly

--- claudio/js/main.js
+++ claudio/js/main.js
@@ -3,47 +3,70 @@
 (function() {
   // do any pre-rdio setup you need to here.
   var _nc = window.MyRdioApp;
-  window.MyRdioApp = {
+  window.Claudio= {
     noConflict: _nc,
+    
+    _connectRdioButton: null,
+    _connectSoundcloudButton: null,
 
     onRdioReady: function() {
-      var readyDiv = document.createElement('div');
-      var readyText = document.createTextNode('Rdio is ready!');
-      readyDiv.appendChild(readyText);
-      document.getElementsByTagName('body')[0].appendChild(readyDiv);
+      var self = this;
+      $('.loading').hide();
+
+      this._connectRdioButton = $('.connect_button.rdio');
+      this._connectRdioButton.click(function() {
+        R.authenticate(self.onRdioAuthenticated);
+      });
+      this._connectSoundcloudButton = $('.connect_button.soundcloud');
+
+      if (R.authenticated()) {
+        this.onRdioAuthenticated(true);
+      } else {
+        this._connectRdioButton.show();
+      }
+    },
+
+    onRdioAuthenticated: function(success) {
+      var self = window.Claudio;
+      if (success) {
+        self.rdioConnected = true;
+        self._connectRdioButton.hide();
+        self.setupSoundcloud();
+      }
+    },
+
+    setupSoundcloud: function() {
+      SC.initialize({
+        client_id: 'f91b39247fdbb16f83ce9ca8780898c8',
+        redirect_uri: 'http://localhost:8888/claudio/callback.html'
+      });
+      this._connectSoundcloudButton.show().click(this.onSoundcloudConnectClicked);
+    },
+
+    onSoundcloudConnectClicked: function() {
+      SC.connect(window.Claudio.authProcessesCompleted);
+    },
+
+    authProcessesCompleted: function() {
+      var self = window.Claudio;
+      self._connectSoundcloudButton.hide();
     }
   };
 
   var _sc_noconflict = window.MySoundcloudApp;
   window.MySoundcloudApp = {
     setup: function() {
-      SC.initialize({
-        client_id: 'f91b39247fdbb16f83ce9ca8780898c8',
-        redirect_uri: 'http://localhost:8888/claudio/callback.html'
-      });
-
-      var connectButton = document.createElement('button');
-      var buttonText = document.createTextNode('Connect Soundcloud');
-      connectButton.appendChild(buttonText);
-      document.getElementsByTagName('body')[0].appendChild(connectButton);
-      connectButton.onclick = this.onConnectClicked;
     },
 
     onConnectClicked: function() {
       //
-      SC.connect(function() {
-        SC.get('/me', function(me) { 
-          console.log('Hello, ' + me.username); 
-        });
-      });
     }
     
   };
 
-  R.ready(function(){
-    window.MyRdioApp.onRdioReady();
-
-    // initialize soundcloud after Rdio is ready so that we know document is also ready
-    window.MySoundcloudApp.setup();
+  $('document').ready(function() {
+    R.ready(function(){
+      window.Claudio.onRdioReady();
+    });
   });
 })();
