166348b09aceaf961bf33a16cc2b32b39368b56a
R. Kevin Nelson <kevin@rkn.la>
2014-04-28 15:29:05 -0700

    Make adding to collection work.

--- catlog/js/catlog.js
+++ catlog/js/catlog.js
@@ -9,7 +9,7 @@
     console.log('authentication!');
 
     CatLog.renderForm();
-    CatLog.renderPlaylist();
+    CatLog.renderLabel();
 
     R.ready(function() {
       R.request({
@@ -39,6 +39,8 @@
   };
 
   CatLog.searchForLabel = function(query) {
+    this.labelInfo = {};
+    this.labelAlbums = [];
     console.log('should search for ' + query);
     R.request({
       method: 'getObjectFromUrl',
@@ -84,112 +86,53 @@
         if (nextOffset < res.result.total) {
           CatLog.populateAlbumsForLabel(labelKey, nextOffset);
         } else {
-          console.log("Got label info: ", CatLog.labelInfo, CatLog.labelAlbums);
+          CatLog.renderLabel(CatLog.labelInfo, CatLog.labelAlbums);
         }
       }
     });
   };
 
-  var cloneSuccessful = function(res) {
-    console.log('playlist cloned!');
+  var addAlbums = function(albums) {
+    var albumKeys = [];
+    _.each(albums, function(el, ix, list) {
+      albumKeys.push(el.key);
+    });
 
-    CatLog.renderPlaylist();
-    $('.label_search_input :first').val('');
+    console.log(albumKeys.join(','));
 
-    var text = '<h3>Cloned successfuly!</h3>';
-    $('.clone-results :first').html(text).fadeIn(200).fadeOut(3000);
+    R.request({
+      method: 'addToCollection',
+      content: {
+        keys: albumKeys.join(','),
+      },
+      success: function(res) {
+        alert("succeeded in adding to your collection");
+      },
+      error: function(res) {
+        alert("error adding to your collection!");
+      }
+    });
   };
 
-  CatLog.handleCloneClicked = function(e) {
-    console.log('clone clicked!');
+  CatLog.renderLabel = function(info, albums) {
+    console.log("Rendering info: ", CatLog.labelInfo, CatLog.labelAlbums);
+    var labelSection = $('section.sourceLabel :first');
 
-    // prepare the track keys
-    var pl = CatLog.currentPlaylist;
-    var trackKeysArray = [];
-
-    $.each(pl.tracks, function(ix, track) {
-      trackKeysArray[ix] = track.key;
-    });
-    var tracksKeys = trackKeysArray.join(',');
-
-    var destination = $('.playlist-destination-selector').val();
-
-    if (destination == "new") {
-      R.request({
-        method: 'createPlaylist',
-        content: {
-          name: pl.name,
-          description: pl.description,
-          tracks: tracksKeys
-        },
-        success: cloneSuccessful,
-        error: function(res) {
-          console.log('error cloning playlist');
-          console.log(res);
-          var text = "Error! ";
-          text += res.message;
-          $('.clone-results :first').html(text).show(400);
-        }
-      });
-    } else {
-      // destination is a playlist
-      // clear the existing one
-      var length = $('.existing_playlist[value=' + destination + ']').first().data().rPlaylistLength;
-
-      var addNewTracks = function() {
-        R.request({
-          method: 'addToPlaylist',
-          content: {
-            playlist: destination,
-            tracks: tracksKeys
-          },
-          success: cloneSuccessful,
-          error: function(res) {
-            console.log('error updating existing playlist. well, this is bad');
-            console.log(res);
-          }
-        });
-      };
-
-      var removeExistingTracks = function(tracksArray) {
-        R.request({
-          method: 'removeFromPlaylist',
-          content: {
-            playlist: destination,
-            index: 0,
-            count: length,
-            tracks: tracksArray.join(',')
-          },
-          success: function(res) {
-            addNewTracks();
-          },
-          error: function(res) {
-            console.log("error removing tracks from existing playlist, bailing");
-            console.log(res);
-          }
-        });
-      };
-
-      if (length > 0) {
-        R.request({
-          method: 'get',
-          content: {
-            keys: destination,
-            extras: '-*,trackKeys'
-          },
-          success: function(res) {
-            removeExistingTracks(res.result[destination].trackKeys);
-          },
-          error: function(res) {
-            console.log("error getting tracks from existing playlist, bailing");
-            console.log(res);
-          }
-        });
-        
-      } else {
-        addNewTracks();
-      }
+    if (info === undefined) {
+      labelSection.empty();
+      return;
     }
+
+    info.albumCount = albums.length;
+
+    var labelInfoSource = $('#labelInfoTemplate').html();
+    var labelInfoTemplate = Handlebars.compile(labelInfoSource);
+    var labelInfoContents = labelInfoTemplate(info);
+    labelSection.empty().append(labelInfoContents);
+
+    $('.add_albums_button').click(function() {
+      addAlbums(albums);
+    });
   };
 
   CatLog.renderForm = function() {
@@ -199,42 +142,5 @@
     ui.empty().append(Handlebars.compile(source)());
     $('.label_search_button :first').click(CatLog.handleSearchEvent);
     $('.clone-results :first').fadeOut();
-  };
-
-  CatLog.renderPlaylist = function(playlist) {
-    var playlistSection = $('section.sourcePlaylist :first');
-
-    if (playlist === undefined) {
-      playlistSection.empty();
-      return;
-    }
-    console.log(playlist);
-
-    CatLog.currentPlaylist = playlist;
-
-    var playlistSource = $('#playlistTemplate').html();
-    var playlistTemplate = Handlebars.compile(playlistSource);
-    var playlistContents = playlistTemplate(playlist);
-    playlistSection.empty().append(playlistContents);
-
-    $('.clone_playlist :first').click(CatLog.handleCloneClicked);
-
-    var playlistOL = $('.playlist-tracks :first').children('ol');
-
-    var trackSource = $('#playlistTrackTemplate').html();
-    var trackTemplate = Handlebars.compile(trackSource);
-    $.each(playlist.tracks, function(i, track) {
-      console.log(i, track);
-      playlistOL.append(trackTemplate(track));
-    });
-
-    var destinationChoices = $('.playlist-destination-selector');
-
-    var destinationItemSource = $('#playlistDestinationItemTemplate').html();
-    var destinationItemTemplate = Handlebars.compile(destinationItemSource);
-    $.each(CatLog.userPlaylists, function(i, pl) {
-      console.log(i, pl);
-      destinationChoices.append(destinationItemTemplate(pl));
-    });
   };
 })();
